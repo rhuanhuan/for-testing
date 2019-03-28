b7f66a9a22eca4d50287b39db1639894b558a126
rhuanhuan <1210098674@qq.com>
2019-03-28 14:01:11 +0800

    [hruan] enhance run.sh

--- run.sh
+++ run.sh
@@ -7,8 +7,8 @@ echo ${CODE_STATUS}
 if [[ ${CODE_STATUS} != *"up to date"* ]]; then
     echo "Code updating"
     git pull --rebase
-    docker stop user-testing
-    docker rm user-testing
+    docker stop user-testing || echo "Container Not found "
+    docker rm user-testing || echo "Container Not found "
     docker build -t usertesting:1 .
     docker run -d --name user-testing -p 3000:3000 usertesting:1
 fi
